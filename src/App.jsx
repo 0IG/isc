9d3f5f60aefcbd0795f0d150638f62619358384f
Isaac <imisaacgonzalez@gmail.com>
2025-01-01 18:17:43 -0500

    Gen: Added routes / Page Not Found (PNF)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
-
+import Home from './components/home/Home'
+import PageNotFound from './components/page-not-found/PageNotFound'
 export default function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<PageNotFound/>}></Route>      
+      </Routes>
     </div>
   )
 }
---
+++ src/components/page-not-found/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./PageNotFound.scss"
+export default function PageNotFound() {
+  return (
+    <div className='PNF'>
+      <h1 className='PNF__heading'>Page Not Found !</h1>
+      <img src='https://pa1.aminoapps.com/6764/61f0290c09c7f2aaa400412d9dd7180297254d43_hq.gif'></img>
+    </div>
+  )
+}
