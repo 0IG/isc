3b692a71c61b02cc925c89e54eb81fe8a1fde838
Isaac <imisaacgonzalez@gmail.com>
2025-01-01 22:51:41 -0500

    Save: Quick save

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Home.scss'
+
 export default function Home() {
+  const scrollToProjects = () => {
+    document.querySelector('.home__projects').scrollIntoView({ 
+      behavior: 'smooth'
+    });
+  }
+
   return (
     <div className='home'>
-        <section className='home__projects'></section>
+        <section className='home__introduction'>
+            <h3 className='home__introduction__name'>ISAAC GONZALEZ</h3>
+            <h1 className='home__introduction__about'>
+                Computer Enthusiast. Brazilian Jiu Jitsu Practitioner. Technician @ BlackRock
+                for AV-Services
+            </h1>
+            <button 
+              className='home__introduction__nav-projects-text'
+              onClick={scrollToProjects}
+            >
+              Check out what I'm up to.
+            </button>
+        </section>
+        <section className='home__projects' id="projects">
+            PROJECTS
+        </section>
         <section className='home__skills'></section>
         <section className='home__contact'>
             <h2>Contact</h2>
@@ -13,6 +35,6 @@ export default function Home() {
             </h4>
             <h3>Linkedin</h3>
         </section>
-        </div>
+    </div>
   )
 }
