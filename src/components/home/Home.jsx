64297aade5d0dceade5f2cfe46d67f784d37379b
Isaac <imisaacgonzalez@gmail.com>
2025-01-01 20:20:25 -0500

    Save: Quick save

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import './Home.scss'
 export default function Home() {
   return (
-    <div></div>
+    <div className='home'>
+        <section className='home__projects'></section>
+        <section className='home__skills'></section>
+        <section className='home__contact'>
+            <h2>Contact</h2>
+            <h4 className='home__contact__text'>
+                I'm always looking to connect and grow not just my network but friends!
+                Feel free to use the links below to reach out!
+            </h4>
+            <h3>Linkedin</h3>
+        </section>
+        </div>
   )
 }
